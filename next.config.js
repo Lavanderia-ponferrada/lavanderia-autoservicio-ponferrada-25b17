d7ba2fb9559b05af8f44fd057f20e37cbc1f294f
Stackbit Code Editor <projects@stackbit.com>
2022-05-04 18:24:22 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withBundleAnalyzer({
     eslint: {
         // Allow production builds to successfully complete even if your project has ESLint errors.
     i18n: {
-        locales: ['default', 'en',],
+        locales: ['default', 'en'],
         defaultLocale: 'default',
         localeDetection: false,
   },
