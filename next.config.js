2750f0cef4294f017113551042ae9dafdaac7339
Stackbit Code Editor <projects@stackbit.com>
2022-05-04 17:26:52 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 sourcebit.fetch(sourcebitConfig);
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  reactStrictMode: true,
-},
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    reactStrictMode: true,
+};
 module.exports = withBundleAnalyzer({
     trailingSlash: true,
     devIndicators: {
