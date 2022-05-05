45e72a2562acc7e7388ff788003052b62fff993a
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:11:01 +0000

    _document.js: changed

--- content/pages/_document.js
+++ content/pages/_document.js
@@ -1,5 +1,5 @@
 import Document from "document";
-import {Html, Head, Main, NextScript } from "next/document";
+import {Html, Head, Main, NextScript } from "document";
 class AppDocument extends Document {
     render() {
         return(
